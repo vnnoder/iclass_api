8de87a625dd69692d25025a32578dd986750d0c1
vnnoder <mitanac@gmail.com>
2013-05-01 13:51:19 +0800

    add has_many relationship

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,5 @@
 class Session < ActiveRecord::Base
   belongs_to :user
   attr_accessible :description, :end_date, :name, :start_date, :status, :user_id
+  has_many :questions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :created_at, :name, :password, :updated_at, :username
+  has_many :questions
+  has_many :sessions
 end
